06ddcfd2b6e061c75cd3caca53096572e923fe4f
larycoder <lenhuchuhiep99@gmail.com>
2023-11-23 13:44:34 +0700

    test: prepare test and monitor which are used to test controller

---
+++ test/loop-20.py
@@ -0,0 +1,6 @@
+import time
+
+if __name__ == "__main__":
+    for i in range(20):
+        print(f"Loop circle: {i}")
+        time.sleep(0.1)
---
+++ test/monitor.py
@@ -0,0 +1,17 @@
+import argparse
+import os
+import time
+
+# Initialize parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--pid", help="target process pid", required=True)
+arg = parser.parse_args()
+pid = arg.pid
+
+if __name__ == "__main__":
+    path = f"/proc/{pid}"
+    print("Monitor start.")
+    while os.path.isdir(path):
+        print("Process is existed.")
+        time.sleep(1)
+    print("Monitor end.")
