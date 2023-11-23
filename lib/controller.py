957e3fbd3226140633fb77e67499816fe711880f
larycoder <lenhuchuhiep99@gmail.com>
2023-11-23 16:38:49 +0700

    exprpy: [wip] simple unix communication between controller and client

--- lib/controller.py
+++ lib/controller.py
@@ -1,15 +1,44 @@
+from .unix_server import UnixServer
 import subprocess
+import socket
+import os
 
 
 class Controller:
     """
     Controller is responsible to spawn another objects.
+    This class acts as unix socket server.
     """
 
-    def __init__(self, test_path, monitor_path, env_path):
+    def __init__(
+        self,
+        sock_path,
+        test_path,
+        monitor_path,
+        env_path,
+    ):
         self.test_home = test_path
         self.monitor_home = monitor_path
         self.env_home = env_path
+
+        # unix socket generation
+        try:
+            os.unlink(sock_path)
+        except OSError:
+            if os.path.exists(sock_path):
+                raise
+
+        try:
+            self.sock = socket.socket(
+                socket.AF_UNIX,
+                socket.SOCK_STREAM,
+            )
+            self.sock.bind(sock_path)
+            self.sock.listen(10)  # listen up to 10 users
+            self.server = UnixServer(self.sock)
+        except Exception as e:
+            print("Could not generate socket server:")
+            raise e
 
     def __exec(self, cmd):
         subprocess.run(cmd, shell=True)
@@ -22,3 +51,8 @@ class Controller:
 
     def env_spawn(self, cmd):
         self.__exec(f"{self.env_home}/{cmd}")
+
+    def server_run(self):
+        while self.server.conn_wait():
+            data = self.server.raw_get_close()
+            print(f"[DEMO] raw: {data}")
--- simple-test.py
+++ simple-test.py
@@ -1,10 +1,44 @@
 from lib.controller import Controller
+import os
+import time
+import sys
+import socket
+import signal
+
+
+def main_test(sock):
+    sock.sendall(b"Hello World")
+
 
 if __name__ == "__main__":
+    mock_sock = "/tmp/hieplnc"
     mock_arg = {
+        "sock_path": mock_sock,
         "test_path": "./test",
         "monitor_path": "./test",
         "env_path": "./test",
     }
-    control = Controller(**mock_arg)
-    control.test_spawn("loop-20.py")
+
+    # child - controller process
+    pid = os.fork()
+    if pid < 0:
+        raise Exeception("Could not fork test to controller process.")
+    elif pid == 0:
+        control = Controller(**mock_arg)
+        control.server_run()
+        sys.exit()
+    else:
+        time.sleep(1)  # wait for controller
+
+    client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    try:
+        client.connect(mock_sock)
+        main_test(client)
+    except Exception as e:
+        print(f"Error to open connection to controller: {e}")
+        os.kill(pid, signal.SIGKILL)
+    finally:
+        client.close()
+
+    # Wait for controller to die
+    os.wait()
